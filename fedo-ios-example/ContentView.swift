cac8235f66cf0de0d01d43868580c023328a3d26
MABD <mabd.universe@gmail.com>
2026-09-15 10:18:40 +0300

    initial commit

---
+++ fedo-ios-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fedo-ios-example
+//
+//  Created by MABD on 15/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fedo-ios-example/fedo_ios_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  fedo_ios_exampleApp.swift
+//  fedo-ios-example
+//
+//  Created by MABD on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct fedo_ios_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
